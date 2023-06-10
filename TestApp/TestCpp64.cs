45b4f2a3b65125f65079464aceb770dedcacd6f9
SGrottel <sgrottel@sgrottel.de>
2023-06-10 13:36:12 +0200

    TestApp now testing cpp lib as well

---
+++ TestApp/TestCpp64.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogTest
+{
+	[TestClass]
+	public class TestCpp64
+	{
+		[TestMethod]
+		public void OneLogFile()
+		{
+			TestImpl.OneLogFile(ExeManager.TestCpp64, false);
+		}
+
+		[TestMethod]
+		public void OneLogFileWaiting()
+		{
+			TestImpl.OneLogFile(ExeManager.TestCpp64, true);
+		}
+
+		[TestMethod]
+		public void ManyLogFiles()
+		{
+			TestImpl.ManyLogFiles(ExeManager.TestCpp64);
+		}
+
+		[TestMethod]
+		public void MultiProcessLogFiles()
+		{
+			TestImpl.MultiProcessLogFiles(ExeManager.TestCpp64, ExeManager.TestCpp64);
+		}
+
+		[TestMethod]
+		public void MultiProcessLogFilesToDelete()
+		{
+			TestImpl.MultiProcessLogFilesToDelete(ExeManager.TestCpp64);
+		}
+	}
+}
